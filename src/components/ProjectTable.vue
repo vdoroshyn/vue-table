1f8d5cbc89c2e96537458461f4faf9896f665f66
vdoroshyn <kjioyh9@gmail.com>
2018-11-09 17:50:50 +0200

    first commit after bootstrap-vue was confirmed as prohibited

---
+++ src/components/ProjectTable.vue
@@ -0,0 +1,70 @@
+<template>
+  <h1>{{test}}</h1>
+</template>
+
+<script>
+export default {
+  name: "ProjectTable",
+  data: function() {
+    return {
+      test: "lel",
+      tableTitles: {
+        name: "Name",
+        height: "Height",
+        mass: "Mass",
+        hair_color: "Hair color",
+        skin_color: "Skin color",
+        eye_color: "Eye color",
+        birth_year: "Birth year",
+        gender: "Gender"
+      },
+      tableData: [
+        {
+          name: "Luke Skywalker",
+          height: "172",
+          mass: "77",
+          hair_color: "blond",
+          skin_color: "fair",
+          eye_color: "blue",
+          birth_year: "19BBY",
+          gender: "male"
+        },
+        {
+          name: "C-3PO",
+          height: "167",
+          mass: "75",
+          hair_color: "n/a",
+          skin_color: "gold",
+          eye_color: "yellow",
+          birth_year: "112BBY",
+          gender: "n/a"
+        },
+        {
+          name: "R2-D2",
+          height: "96",
+          mass: "32",
+          hair_color: "n/a",
+          skin_color: "white, blue",
+          eye_color: "red",
+          birth_year: "33BBY",
+          gender: "n/a"
+        },
+        {
+          name: "Darth Vader",
+          height: "202",
+          mass: "136",
+          hair_color: "none",
+          skin_color: "white",
+          eye_color: "yellow",
+          birth_year: "41.9BBY",
+          gender: "male"
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
