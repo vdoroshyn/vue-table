8cb37129562effd4ddc105ba054b1778e2489005
vdoroshyn <kjioyh9@gmail.com>
2018-11-11 14:33:13 +0200

    props are sent from the parent component to the child one, everything seems stable, removed hardcode in filter array, there are still old bugs thou

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ProjectTable />
+    <ProjectTable v-bind:tableTitles="tableTitles" v-bind:tableData="tableData"/>
   </div>
 </template>
 
@@ -11,6 +11,52 @@ export default {
   name: "app",
   components: {
     ProjectTable
+  },
+  data: function() {
+    return {
+      tableTitles: {
+        name: "Name",
+        height: "Height",
+        mass: "Mass",
+        hair_color: "Hair color",
+        skin_color: "Skin color",
+        eye_color: "Eye color",
+        birth_year: "Birth year",
+        gender: "Gender"
+      },
+      tableData: [
+        {
+          name: "Luke Skywalker",
+          height: "172",
+          mass: "77",
+          hair_color: "blond",
+          skin_color: "fair",
+          eye_color: "blue",
+          birth_year: "19BBY",
+          gender: "male"
+        },
+        {
+          name: "C-3PO",
+          height: "167",
+          mass: "75",
+          hair_color: "n/a",
+          skin_color: "gold",
+          eye_color: "yellow",
+          birth_year: "112BBY",
+          gender: "n/a"
+        },
+        {
+          name: "R2-D2",
+          height: "96",
+          mass: "32",
+          hair_color: "n/a",
+          skin_color: "white, blue",
+          eye_color: "red",
+          birth_year: "33BBY",
+          gender: "n/a"
+        }
+      ]
+    };
   }
 };
 </script>
