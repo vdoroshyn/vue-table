0b283b32556c36a4d1f2ec5798d2db92eedd048f
vdoroshyn <kjioyh9@gmail.com>
2018-11-11 19:14:33 +0200

    the component became more reusable, the majority of hardcode was removed, the sorting bug was fixed

--- src/components/ProjectTable.vue
+++ src/components/ProjectTable.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div>
-      <div class="checkbox" v-for="item in tableTitles" v-bind:key="item">
+      <div class="checkbox" v-for="item in initialData.titles" v-bind:key="item">
         <label>{{item}}
           <input type="checkbox" v-bind:value="item" v-model="filterCategories"/>
         </label>
@@ -10,45 +10,36 @@
     <table>
       <thead>
         <tr>
-          <th v-for="(item, key, index) in tableTitles" v-show="filterCategories.includes(item)" @click="sort(key)" v-bind:key="index">{{item}}</th>
+          <th v-for="(item, key, index) in initialData.titles" v-show="filterCategories.includes(item)" @click="sort(key)" v-bind:key="index">{{item}}</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="(value, index) in sortedTableData" v-bind:key="index">
-          <td v-show="filterCategories.includes('Name') && EditableName!==index" @dblclick="EditableName=index">{{value.name}}</td>
+          <td v-show="filterCategories.includes('Name') && EditableName!==index" @dblclick="EditableName=index; sortBy=''">{{value.name}}</td>
           <input v-show="EditableName===index" type="text" v-model="value.name" v-on:blur="EditableName=null" @keyup.enter="EditableName=null">
 
-          <td v-show="filterCategories.includes('Height') && EditableHeight!==index" @dblclick="EditableHeight=index">{{value.height}}</td>
+          <td v-show="filterCategories.includes('Height') && EditableHeight!==index" @dblclick="EditableHeight=index; sortBy=''">{{value.height}}</td>
           <input v-show="EditableHeight===index" type="text" v-model="value.height" v-on:blur="EditableHeight=null" @keyup.enter="EditableHeight=null">
 
-          <td v-show="filterCategories.includes('Mass') && EditableMass!==index" @dblclick="EditableMass=index">{{value.mass}}</td>
+          <td v-show="filterCategories.includes('Mass') && EditableMass!==index" @dblclick="EditableMass=index; sortBy=''">{{value.mass}}</td>
           <input v-show="EditableMass===index" type="text" v-model="value.mass" v-on:blur="EditableMass=null" @keyup.enter="EditableMass=null">
 
-          <td v-show="filterCategories.includes('Hair color') && EditableHairColor!==index" @dblclick="EditableHairColor=index">{{value.hair_color}}</td>
+          <td v-show="filterCategories.includes('Hair color') && EditableHairColor!==index" @dblclick="EditableHairColor=index; sortBy=''">{{value.hair_color}}</td>
           <input v-show="EditableHairColor===index" type="text" v-model="value.hair_color" v-on:blur="EditableHairColor=null" @keyup.enter="EditableHairColor=null">
 
-          <td v-show="filterCategories.includes('Skin color') && EditableSkinColor!==index" @dblclick="EditableSkinColor=index">{{value.skin_color}}</td>
+          <td v-show="filterCategories.includes('Skin color') && EditableSkinColor!==index" @dblclick="EditableSkinColor=index; sortBy=''">{{value.skin_color}}</td>
           <input v-show="EditableSkinColor===index" type="text" v-model="value.skin_color" v-on:blur="EditableSkinColor=null" @keyup.enter="EditableSkinColor=null">
 
-          <td v-show="filterCategories.includes('Eye color') && EditableEyeColor!==index" @dblclick="EditableEyeColor=index">{{value.eye_color}}</td>
+          <td v-show="filterCategories.includes('Eye color') && EditableEyeColor!==index" @dblclick="EditableEyeColor=index; sortBy=''">{{value.eye_color}}</td>
           <input v-show="EditableEyeColor===index" type="text" v-model="value.eye_color" v-on:blur="EditableEyeColor=null" @keyup.enter="EditableEyeColor=null">
 
-          <td v-show="filterCategories.includes('Birth year') && EditableBirthYear!==index" @dblclick="EditableBirthYear=index">{{value.birth_year}}</td>
+          <td v-show="filterCategories.includes('Birth year') && EditableBirthYear!==index" @dblclick="EditableBirthYear=index; sortBy=''">{{value.birth_year}}</td>
           <input v-show="EditableBirthYear===index" type="text" v-model="value.birth_year" v-on:blur="EditableBirthYear=null" @keyup.enter="EditableBirthYear=null">
 
-          <td v-show="filterCategories.includes('Gender') && EditableGender!==index" @dblclick="EditableGender=index">{{value.gender}}</td>
+          <td v-show="filterCategories.includes('Gender') && EditableGender!==index" @dblclick="EditableGender=index; sortBy=''">{{value.gender}}</td>
           <input v-show="EditableGender===index" type="text" v-model="value.gender" v-on:blur="EditableGender=null" @keyup.enter="EditableGender=null">
-          <!-- <td v-show="filterCategories.includes('Name')">{{value.name}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Height')">{{value.height}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Mass')">{{value.mass}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Hair color')">{{value.hair_color}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Skin color')">{{value.skin_color}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Eye color')">{{value.eye_color}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Birth year')">{{value.birth_year}}</td> -->
-          <!-- <td v-show="filterCategories.includes('Gender')">{{value.gender}}</td> -->
         </tr>
       </tbody>
-      <!-- debug: sort={{sortBy}}, dir={{sortOrder}}, filterArray={{filterCategories}} -->
     </table>
   </div>
 </template>
@@ -56,7 +47,7 @@
 <script>
 export default {
   name: "ProjectTable",
-  props: ["tableTitles", "tableData"],
+  props: ["initialData"],
   methods: {
     sort(item) {
       item = item.toLowerCase();
@@ -74,13 +65,13 @@ export default {
     /*
     **this is done for the user to see every table column from the start
     */
-    for (let value in this.tableTitles) {
-      this.filterCategories.push(this.tableTitles[value]);
+    for (let value in this.initialData.titles) {
+      this.filterCategories.push(this.initialData.titles[value]);
     }
   },
   computed: {
     sortedTableData: function() {
-      return this.tableData.sort((a, b) => {
+      return this.initialData.items.sort((a, b) => {
         const item = this.sortBy;
         const orderMod = this.sortOrder === "desc" ? -1 : 1;
         /*
@@ -126,7 +117,7 @@ export default {
   },
   data: function() {
     return {
-      sortBy: "name",
+      sortBy: "",
       sortOrder: "asc",
       EditableName: null,
       EditableHeight: null,
@@ -137,324 +128,11 @@ export default {
       EditableBirthYear: null,
       EditableGender: null,
       filterCategories: []
-      // tableTitles: {
-      //   name: "Name",
-      //   height: "Height",
-      //   mass: "Mass",
-      //   hair_color: "Hair color",
-      //   skin_color: "Skin color",
-      //   eye_color: "Eye color",
-      //   birth_year: "Birth year",
-      //   gender: "Gender"
-      // },
-      // tableData: [
-      //   {
-      //     name: "Luke Skywalker",
-      //     height: "172",
-      //     mass: "77",
-      //     hair_color: "blond",
-      //     skin_color: "fair",
-      //     eye_color: "blue",
-      //     birth_year: "19BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "C-3PO",
-      //     height: "167",
-      //     mass: "75",
-      //     hair_color: "n/a",
-      //     skin_color: "gold",
-      //     eye_color: "yellow",
-      //     birth_year: "112BBY",
-      //     gender: "n/a"
-      //   },
-      //   {
-      //     name: "R2-D2",
-      //     height: "96",
-      //     mass: "32",
-      //     hair_color: "n/a",
-      //     skin_color: "white, blue",
-      //     eye_color: "red",
-      //     birth_year: "33BBY",
-      //     gender: "n/a"
-      //   },
-      //   {
-      //     name: "Darth Vader",
-      //     height: "202",
-      //     mass: "136",
-      //     hair_color: "none",
-      //     skin_color: "white",
-      //     eye_color: "yellow",
-      //     birth_year: "41.9BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Leia Organa",
-      //     height: "150",
-      //     mass: "49",
-      //     hair_color: "brown",
-      //     skin_color: "light",
-      //     eye_color: "brown",
-      //     birth_year: "19BBY",
-      //     gender: "female"
-      //   },
-      //   {
-      //     name: "Owen Lars",
-      //     height: "178",
-      //     mass: "120",
-      //     hair_color: "brown, grey",
-      //     skin_color: "light",
-      //     eye_color: "blue",
-      //     birth_year: "52BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Beru Whitesun lars",
-      //     height: "165",
-      //     mass: "75",
-      //     hair_color: "brown",
-      //     skin_color: "light",
-      //     eye_color: "blue",
-      //     birth_year: "47BBY",
-      //     gender: "female"
-      //   },
-      //   {
-      //     name: "R5-D4",
-      //     height: "97",
-      //     mass: "32",
-      //     hair_color: "n/a",
-      //     skin_color: "white, red",
-      //     eye_color: "red",
-      //     birth_year: "unknown",
-      //     gender: "n/a"
-      //   },
-      //   {
-      //     name: "Biggs Darklighter",
-      //     height: "183",
-      //     mass: "84",
-      //     hair_color: "black",
-      //     skin_color: "light",
-      //     eye_color: "brown",
-      //     birth_year: "24BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Obi-Wan Kenobi",
-      //     height: "182",
-      //     mass: "77",
-      //     hair_color: "auburn, white",
-      //     skin_color: "fair",
-      //     eye_color: "blue-gray",
-      //     birth_year: "57BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Anakin Skywalker",
-      //     height: "188",
-      //     mass: "84",
-      //     hair_color: "blond",
-      //     skin_color: "fair",
-      //     eye_color: "blue",
-      //     birth_year: "41.9BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Wilhuff Tarkin",
-      //     height: "180",
-      //     mass: "unknown",
-      //     hair_color: "auburn, grey",
-      //     skin_color: "fair",
-      //     eye_color: "blue",
-      //     birth_year: "64BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Chewbacca",
-      //     height: "228",
-      //     mass: "112",
-      //     hair_color: "brown",
-      //     skin_color: "unknown",
-      //     eye_color: "blue",
-      //     birth_year: "200BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Han Solo",
-      //     height: "180",
-      //     mass: "80",
-      //     hair_color: "brown",
-      //     skin_color: "fair",
-      //     eye_color: "brown",
-      //     birth_year: "29BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Greedo",
-      //     height: "173",
-      //     mass: "74",
-      //     hair_color: "n/a",
-      //     skin_color: "green",
-      //     eye_color: "black",
-      //     birth_year: "44BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Jabba Desilijic Tiure",
-      //     height: "175",
-      //     mass: "1,358",
-      //     hair_color: "n/a",
-      //     skin_color: "green-tan, brown",
-      //     eye_color: "orange",
-      //     birth_year: "600BBY",
-      //     gender: "hermaphrodite"
-      //   },
-      //   {
-      //     name: "Wedge Antilles",
-      //     height: "170",
-      //     mass: "77",
-      //     hair_color: "brown",
-      //     skin_color: "fair",
-      //     eye_color: "hazel",
-      //     birth_year: "21BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Jek Tono Porkins",
-      //     height: "180",
-      //     mass: "110",
-      //     hair_color: "brown",
-      //     skin_color: "fair",
-      //     eye_color: "blue",
-      //     birth_year: "unknown",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Yoda",
-      //     height: "66",
-      //     mass: "17",
-      //     hair_color: "white",
-      //     skin_color: "green",
-      //     eye_color: "brown",
-      //     birth_year: "896BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Palpatine",
-      //     height: "170",
-      //     mass: "75",
-      //     hair_color: "grey",
-      //     skin_color: "pale",
-      //     eye_color: "yellow",
-      //     birth_year: "82BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Boba Fett",
-      //     height: "183",
-      //     mass: "78.2",
-      //     hair_color: "black",
-      //     skin_color: "fair",
-      //     eye_color: "brown",
-      //     birth_year: "31.5BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "IG-88",
-      //     height: "200",
-      //     mass: "140",
-      //     hair_color: "none",
-      //     skin_color: "metal",
-      //     eye_color: "red",
-      //     birth_year: "15BBY",
-      //     gender: "none"
-      //   },
-      //   {
-      //     name: "Bossk",
-      //     height: "190",
-      //     mass: "113",
-      //     hair_color: "none",
-      //     skin_color: "green",
-      //     eye_color: "red",
-      //     birth_year: "53BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Lando Calrissian",
-      //     height: "177",
-      //     mass: "79",
-      //     hair_color: "black",
-      //     skin_color: "dark",
-      //     eye_color: "brown",
-      //     birth_year: "31BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Lobot",
-      //     height: "175",
-      //     mass: "79",
-      //     hair_color: "none",
-      //     skin_color: "light",
-      //     eye_color: "blue",
-      //     birth_year: "37BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Ackbar",
-      //     height: "180",
-      //     mass: "83",
-      //     hair_color: "none",
-      //     skin_color: "brown mottle",
-      //     eye_color: "orange",
-      //     birth_year: "41BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Mon Mothma",
-      //     height: "150",
-      //     mass: "unknown",
-      //     hair_color: "auburn",
-      //     skin_color: "fair",
-      //     eye_color: "blue",
-      //     birth_year: "48BBY",
-      //     gender: "female"
-      //   },
-      //   {
-      //     name: "Arvel Crynyd",
-      //     height: "unknown",
-      //     mass: "unknown",
-      //     hair_color: "brown",
-      //     skin_color: "fair",
-      //     eye_color: "brown",
-      //     birth_year: "unknown",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Wicket Systri Warrick",
-      //     height: "88",
-      //     mass: "20",
-      //     hair_color: "brown",
-      //     skin_color: "brown",
-      //     eye_color: "brown",
-      //     birth_year: "8BBY",
-      //     gender: "male"
-      //   },
-      //   {
-      //     name: "Nien Nunb",
-      //     height: "160",
-      //     mass: "68",
-      //     hair_color: "none",
-      //     skin_color: "grey",
-      //     eye_color: "black",
-      //     birth_year: "unknown",
-      //     gender: "male"
-      //   }
-      // ]
     };
   }
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 table {
   font-family: arial, sans-serif;
